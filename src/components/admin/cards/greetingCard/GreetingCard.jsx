cb7d5815e33ce8fb5bf85a64bd17591e2fc0d88c
Kasun Chiwantha <kasunchiwantha789@gmail.com>
2026-02-24 21:39:46 +0530

    updated greeting card

--- src/components/admin/cards/greetingCard/GreetingCard.jsx
+++ src/components/admin/cards/greetingCard/GreetingCard.jsx
@@ -20,20 +20,20 @@ const GreetingCard = () => {
   }
 
   return (
-    <div className="rounded-xl px-4 py-6 bg-white border-blue-200 border shadow-lg flex flex-col space-y-4 justify-center items-center">
+    <div className="border-b border-gray-200 px-4 pb-2 pt-6 flex flex-col space-y-4 justify-center items-center">
       <div
-        className="rounded-full w-[90%]  items-center justify-center flex  
+        className="rounded-full w-[60%]  items-center justify-center flex  
       aspect-square bg-gray-100 text-blue-600"
       >
-        <User size={100} className="animate-pulse" />
+        <User size={80} className="animate-pulse" />
       </div>
       <div className="text-center font-light capitalize text-gray-700 flex flex-col -space-y-1">
         <span className="text-sm">{greeting}</span>
-        <h2 className="line-clamp-1 text-ellipsis text-xl text-gray-700 font-bold">
+        <h2 className="line-clamp-1 uppercase text-ellipsis text-xl text-gray-700 font-bold">
           Mr. Chiwantha
         </h2>
       </div>
-      <div className="flex items-center justify-center gap-2">
+      <div className="items-center justify-center gap-2 hidden">
         <Link
           href={`/users/admin`}
           className="aspect-square  rounded-xl bg-gray-100 hover:bg-gray-200
